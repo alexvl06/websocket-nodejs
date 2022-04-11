855ad88f73b178deef0bf08fb83d95e11a9d1ae7
Alexis <alexis.avila@inter-telco.com>
2022-04-11 00:25:07 -0500

    adding al files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const db = require('./db')
 const router = require('./network/routes')
 
 let app = express();
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router(app);
-
+db('mongodb://root:PZTsIovVD1iXy4jl@cluster0-shard-00-00.3yqqu.mongodb.net:27017,cluster0-shard-00-01.3yqqu.mongodb.net:27017,cluster0-shard-00-02.3yqqu.mongodb.net:27017/platziChat?ssl=true&replicaSet=atlas-3zw6s6-shard-0&authSource=admin&retryWrites=true&w=majority')
 
 app.use('/app',express.static('public'));
 
